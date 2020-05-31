73a750f619970aee84c9da80c853e66423b1caaa
Spronjee <i.chukhlebov6530@gmail.com>
2020-06-01 01:10:09 +0300

    Adding new Vernam cipher

--- Course_work/main.cpp
+++ Course_work/main.cpp
@@ -46,9 +46,13 @@ int main(int argc, const char * argv[]) {
 }
 */
 #include <iostream>
+#include <ctime>
+#include <string>
 #include <fstream>
 
 using namespace std;
+
+
 
 string encryptDecrypt(string toEncrypt) {
     
@@ -63,6 +67,7 @@ string encryptDecrypt(string toEncrypt) {
     return output;
 }
 
+
 int main(int argc, const char * argv[])
 {
     setlocale(LC_ALL, "");
@@ -70,26 +75,81 @@ int main(int argc, const char * argv[])
     cout << "Enter the text u wanna encrypt: "<< endl;
     getline(cin, toEncrypt );
     
-    cout << "Choose the mode of encrypting: \n 1. Modern way \n 2. Vernam \n 3.Gilla \n 4.Gambetta \n 5."<< endl;
+    cout << "Choose the mode of encrypting: \n 1. Modern way \n 2. Vernam \n 3.Gilla \n 4.Gambetta \n 5.Vertical-horizont \n 6.Wodsvord "<< endl;
     int mode;
     cin >> mode;
     switch (mode)
     {
-        case 1:
             
-            cout << "1";
-            break;
+            
+            
+        case 1:
+            cout << "You chose 1 mode: \n";
+            //string encrypted = encryptDecrypt(toEncrypt);
+               cout << "Encrypted:" << encryptDecrypt(toEncrypt)<< "\n";
+              // string decrypted = encryptDecrypt(encrypted);
+               cout << "Decrypted:" << encryptDecrypt(encryptDecrypt(toEncrypt)) << "\n";
+               break;
         case 2:
+            
             cout << "2";
+            string C,D = "";
+            string A = "abcdefghijklmnopqrstuvwxyz";
+            
+            cout << "Enter the key: ";
+            cin >> C;
+            string B = toEncrypt;
+            int *F = new int [B.size()];
+            int *G = new int [B.size()];
+            unsigned long c=C.size();
+            unsigned long b=B.size();
+            if (b>=c)
+            {
+                for (int i =0; i< (b/c); i++)
+                {
+                    D=D+C;
+                }
+                for(int j=0; j<(b%c); j++)
+                {
+                    D=D+C[j];
+                }
+            }
+            else
+            {
+                for (int s=0; s<b; s++)
+                {
+                    D=D+B[s];
+                }
+            }
+            cout << D << endl;
+            for (int k=0; k<b ; k++)
+            {
+                for(int n=0;n <26 ;n++)
+                {
+                    if(B[k]==A[n])
+                    {
+                        F[k] =n;
+                    }
+                
+                if (D[k]==A[n])
+                {
+                    G[k]=n;
+                }
+                }
+            }
+            int e=0;
+            for(int u=0;u <b; u++)
+            {
+                e=((F[u]+G[u])%26);
+                B[u] = A[u];
+            }
+            cout << "Your word: " << B << endl;
+            break;
         default: cout << "Something happened..."<< endl;
             break;
     }
     
-    string encrypted = encryptDecrypt(toEncrypt);
-    cout << "Encrypted:" << encrypted << "\n";
-    
-    string decrypted = encryptDecrypt(encrypted);
-    cout << "Decrypted:" << decrypted << "\n";
+   
     
     return 0;
 }
