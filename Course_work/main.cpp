8f74786263ac6f907e46e48cf6d92c3f807a85d3
Spronjee <i.chukhlebov6530@gmail.com>
2020-06-01 18:08:25 +0300

    Updating interface

--- Course_work/main.cpp
+++ Course_work/main.cpp
@@ -51,12 +51,74 @@ int main(int argc, const char * argv[]) {
 #include <fstream>
 
 using namespace std;
-
-
-
+string secondMethod(string toEncrypt){
+    cout << "Requirements about using this ciepher: \n 1.It can't be too long text \n 2.Write the key without spaces \n 3.The key can't be shorter than text \n";
+string C,D = "";
+           string A = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.()1234567890!;:-? ";
+    //
+           cout << "Enter the key: ";
+           cin >> C;
+    
+    cout << "Decrypted: " << endl;
+           string B = toEncrypt;
+           int *F = new int [B.size()];
+           int *G = new int [B.size()];
+           unsigned long c=C.size();
+           unsigned long b=B.size();
+           if (b>=c)
+           {
+               
+               for (int i =0; i< (b/c); i++)
+               {
+                   D=D+C;
+               }
+               for(int j=0; j<(b%c); j++)
+               {
+                   D=D+C[j];
+               }
+           }
+    
+            
+           else
+           {
+               for (int s=0; s<b; s++)
+               {
+                   D=D+B[s];
+               }
+               
+           }
+               
+           
+           cout << D << endl;
+           for (int k=0; k<b ; k++)
+           {
+               for(int n=0;n <26 ;n++)
+               {
+                   if(B[k]==A[n])
+                   {
+                       F[k] =n;
+                   }
+               
+               if (D[k]==A[n])
+               {
+                   G[k]=n;
+               }
+               }
+           
+           int e=0;
+           for(int u=0;u <b; u++)
+           {
+               e=((F[u]+G[u])%26);
+               B[u] = A[u];
+           }
+           }
+    cout << "Encrypted: " << endl;
+    return B;
+    
+}
 string encryptDecrypt(string toEncrypt) {
     
-    char key[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N'}; //Any chars will work, in an array of any size
+    char key[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N'};
     string output = toEncrypt;
    
     
@@ -73,11 +135,17 @@ int main(int argc, const char * argv[])
     setlocale(LC_ALL, "");
     string toEncrypt;
     cout << "Enter the text u wanna encrypt: "<< endl;
+    
+    
+    ifstream fin;
+    fin.open("text.txt");
     getline(cin, toEncrypt );
     
-    cout << "Choose the mode of encrypting: \n 1. Modern way \n 2. Vernam \n 3.Gilla \n 4.Gambetta \n 5.Vertical-horizont \n 6.Wodsvord "<< endl;
+    cout << "Choose the mode of encrypting: \n 1.Modern way \n 2.Vizhener \n 3.Gilla \n 4.Gambetta \n 5.Vertical-horizont \n 6.Wodsvord "<< endl;
     int mode;
     cin >> mode;
+     ofstream fout;
+    string start,end;
     switch (mode)
     {
             
@@ -85,71 +153,49 @@ int main(int argc, const char * argv[])
             
         case 1:
             cout << "You chose 1 mode: \n";
-            //string encrypted = encryptDecrypt(toEncrypt);
-               cout << "Encrypted:" << encryptDecrypt(toEncrypt)<< "\n";
-              // string decrypted = encryptDecrypt(encrypted);
-               cout << "Decrypted:" << encryptDecrypt(encryptDecrypt(toEncrypt)) << "\n";
-               break;
-        case 2:
-            
-            cout << "2";
-            string C,D = "";
-            string A = "abcdefghijklmnopqrstuvwxyz";
-            
-            cout << "Enter the key: ";
-            cin >> C;
-            string B = toEncrypt;
-            int *F = new int [B.size()];
-            int *G = new int [B.size()];
-            unsigned long c=C.size();
-            unsigned long b=B.size();
-            if (b>=c)
+            start = encryptDecrypt(toEncrypt);
+            end = encryptDecrypt(encryptDecrypt(toEncrypt));
+            cout << "Encrypted: " << start;
+            cout << "Decrypted: " << end;
+              // cout << "Encrypted:" << encryptDecrypt(toEncrypt)<< "\n";
+               //cout << "Decrypted:" << encryptDecrypt(encryptDecrypt(toEncrypt)) << "\n";
+           
+            fout.open("text.txt", std::ios::app);
+            if (!fout.is_open())
             {
-                for (int i =0; i< (b/c); i++)
-                {
-                    D=D+C;
-                }
-                for(int j=0; j<(b%c); j++)
-                {
-                    D=D+C[j];
-                }
+                cout << "Oops..something is wrong" << endl;
             }
             else
             {
-                for (int s=0; s<b; s++)
-                {
-                    D=D+B[s];
-                }
+                fout << start << " " << end;
             }
-            cout << D << endl;
-            for (int k=0; k<b ; k++)
-            {
-                for(int n=0;n <26 ;n++)
-                {
-                    if(B[k]==A[n])
-                    {
-                        F[k] =n;
-                    }
-                
-                if (D[k]==A[n])
-                {
-                    G[k]=n;
-                }
-                }
-            }
-            int e=0;
-            for(int u=0;u <b; u++)
-            {
-                e=((F[u]+G[u])%26);
-                B[u] = A[u];
-            }
-            cout << "Your word: " << B << endl;
+            
+            
+               break;
+        case 2:
+            
+            cout << "You chose 2 mode: \n";
+            cout << secondMethod(toEncrypt) << "\n";
             break;
+            
+        case 3:
+            cout << "You chose 3 mode: \n";
         default: cout << "Something happened..."<< endl;
             break;
     }
     
    
+    
+  //  fout.close();
+  //  ifstream fin;
+    fin.open("text.txt");
+
+    if (!fin.is_open())
+    {
+        cout << "can't be opened" << endl;
+    }
+    
+     fin.close();
     
     return 0;
 }
